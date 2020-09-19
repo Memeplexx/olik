6302db66704b05e649b69585ef1dce8cff084151
stephen <stephensimonpaul@gmail.com>
2020-09-19 19:46:23 +0200

    add new devtools test

--- test/devtools.spec.ts
+++ test/devtools.spec.ts
@@ -6,7 +6,7 @@ describe('Devtools', () => {
 
   beforeAll(() => tests.windowObject = windowAugmentedWithReduxDevtoolsImpl);
 
-  it('should update a top-level object', () => {
+  it('correctly respond to devtools dispatches where the state is an object', () => {
     const initialState = { x: 0, y: 0 };
     const getStore = make('state', initialState);
     getStore(s => s.x).replaceWith(3);
@@ -14,6 +14,18 @@ describe('Devtools', () => {
     const state = { x: 1, y: 0 };
     tests.windowObject?.__REDUX_DEVTOOLS_EXTENSION__._mockInvokeSubscription({ type: 'DISPATCH', state: JSON.stringify(state) });
     expect(getStore().read()).toEqual(state);
+    expect(tests.currentAction.type).toEqual('replace() [dontTrackWithDevtools]');
+  })
+
+  it('correctly respond to devtools dispatches where the state is an array', () => {
+    const initialState = ['a', 'b', 'c'];
+    const getStore = make('state', initialState);
+    getStore().replaceAll(['d', 'e', 'f']);
+    expect(getStore().read()).toEqual(['d', 'e', 'f']);
+    const state = ['g', 'h'];
+    tests.windowObject?.__REDUX_DEVTOOLS_EXTENSION__._mockInvokeSubscription({ type: 'DISPATCH', state: JSON.stringify(state) });
+    expect(getStore().read()).toEqual(state);
+    expect(tests.currentAction.type).toEqual('replace() [dontTrackWithDevtools]');
   })
 
 });
