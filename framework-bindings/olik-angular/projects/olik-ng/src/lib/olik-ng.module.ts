90ae609711c4a314245e93765449b4c81ab4c453
stephen <stephensimonpaul@gmail.com>
2021-08-07 15:11:41 +0200

    Revert previous

--- framework-bindings/olik-angular/projects/olik-ng/src/lib/olik-ng.module.ts
+++ framework-bindings/olik-angular/projects/olik-ng/src/lib/olik-ng.module.ts
@@ -1,28 +1,27 @@
 import { EventEmitter, NgModule, NgZone } from '@angular/core';
 import * as core from 'olik';
-import { FutureState } from 'olik';
 import { combineLatest, from, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 export * from 'olik';
 
-// declare module 'olik' {
-//   interface StoreOrDerivation<C> {
-//     observe: () => Observable<C>;
-//   }
-//   interface ArrayOfElementsCommonAction<X extends core.DeepReadonlyArray<any>, F extends core.FindOrFilter, T extends core.Trackability> {
-//     observe: () => Observable<F extends 'find' ? X[0] : X>;
-//   }
-//   interface Future<C> {
-//     observeStatus: () => Observable<FutureState<C>>;
-//     asObservable: () => Observable<C>;
-//   }
-//   interface Async<C> extends Observable<C> {
-//   }
-//   interface Derivation<R> {
-//     observe: () => Observable<R>;
-//   }
-// }
+declare module 'olik' {
+  interface StoreOrDerivation<C> {
+    observe: () => Observable<C>;
+  }
+  interface ArrayOfElementsCommonAction<X extends core.DeepReadonlyArray<any>, F extends core.FindOrFilter, T extends core.Trackability> {
+    observe: () => Observable<F extends 'find' ? X[0] : X>;
+  }
+  interface Future<C> {
+    observeStatus: () => Observable<core.FutureState<C>>;
+    asObservable: () => Observable<C>;
+  }
+  interface Async<C> extends Observable<C> {
+  }
+  interface Derivation<R> {
+    observe: () => Observable<R>;
+  }
+}
 
 type FunctionParameter<T> = T extends (arg: infer H) => any ? H : never;
 type ClassObservables<T> = {
