9f344d1d2dbacc3c56771cc9dfba1620621ce821
stephen <stephensimonpaul@gmail.com>
2021-08-07 17:25:20 +0200

    ditto

--- framework-bindings/olik-ng/src/core.ts
+++ framework-bindings/olik-ng/src/core.ts
@@ -9,18 +9,18 @@ declare module 'olik' {
   interface StoreOrDerivation<C> {
     observe: () => Observable<C>;
   }
-  interface ArrayOfElementsCommonAction<X extends core.DeepReadonlyArray<any>, F extends core.FindOrFilter, T extends core.Trackability> {
-    observe: () => Observable<F extends 'find' ? X[0] : X>;
-  }
-  interface Future<C> {
-    observeStatus: () => Observable<core.FutureState<C>>;
-    asObservable: () => Observable<C>;
-  }
-  interface Async<C> extends Observable<C> {
-  }
-  interface Derivation<R> {
-    observe: () => Observable<R>;
-  }
+  // interface ArrayOfElementsCommonAction<X extends core.DeepReadonlyArray<any>, F extends core.FindOrFilter, T extends core.Trackability> {
+  //   observe: () => Observable<F extends 'find' ? X[0] : X>;
+  // }
+  // interface Future<C> {
+  //   observeStatus: () => Observable<core.FutureState<C>>;
+  //   asObservable: () => Observable<C>;
+  // }
+  // interface Async<C> extends Observable<C> {
+  // }
+  // interface Derivation<R> {
+  //   observe: () => Observable<R>;
+  // }
 }
 
 // type FunctionParameter<T> = T extends (arg: infer H) => any ? H : never;
