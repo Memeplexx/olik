1f4d4ab18af80a45bc860fb44f38f9dac177c778
stephen <stephensimonpaul@gmail.com>
2021-08-07 17:37:39 +0200

    ditto

--- framework-bindings/olik-ng/src/core.ts
+++ framework-bindings/olik-ng/src/core.ts
@@ -1,8 +1,3 @@
-import { EventEmitter, NgModule, NgZone } from '@angular/core';
-import * as core from 'olik';
-import { combineLatest, from, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-
 export * from 'olik';
 
 declare module 'olik' {
--- framework-bindings/olik-ng/test/angular.spec.ts
+++ framework-bindings/olik-ng/test/angular.spec.ts
@@ -1,14 +1,4 @@
-import { from, of } from 'rxjs';
-import { catchError, skip } from 'rxjs/operators';
-import { Observable } from 'rxjs';
-
-import {
-  // combineComponentObservables,
-  createRootStore,
-  createComponentStore,
-  deriveFrom,
-  // OlikNgModule,
-} from '../src/core';
+import { createRootStore, deriveFrom } from 'olik';
 
 describe('Angular', () => {
 
@@ -17,10 +7,6 @@ describe('Angular', () => {
     array: [{ id: 1, value: 'one' }, { id: 2, value: 'two' }, { id: 3, value: 'three' }],
     string: 'b',
   };
-
-  beforeEach(() => {
-    // new OlikNgModule(null as any);
-  })
 
   it('', () => {
     const select = createRootStore(initialState, { devtools: false });
