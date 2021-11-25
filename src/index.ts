1a07d7f08d90cb03d04d150305edd707ca1ef880
stephen <stephensimonpaul@gmail.com>
2021-11-25 16:01:11 +0200

    Re-organization project structure

--- src/index.ts
+++ src/index.ts
@@ -1,106 +1,5 @@
-export type FindOrFilter = 'isFind' | 'isFilter';
-export type QueryStatus = 'notQueried' | 'queried';
+import { FindOrFilter, StateAction, UpdatableArray, UpdatableObject, UpdatablePrimitive } from "./types";
 
-/**
- * An array which cannot be mutated
- */
-export interface DeepReadonlyArray<T> extends ReadonlyArray<DeepReadonly<T>> {
-}
-
-/**
- * An object which cannot be mutated
- */
-export type DeepReadonlyObject<T> = {
-  readonly [P in keyof T]: DeepReadonly<T[P]>;
-};
-
-/**
- * An object or an array which cannot be mutated
- */
-export type DeepReadonly<T> =
-  T extends (infer R)[] ? DeepReadonlyArray<R> :
-  T extends object ? DeepReadonlyObject<T> :
-  T;
-
-/**
- * An object which can be unsubscribed from
- */
-export interface Unsubscribable {
-  /**
-   * Unsubscribes from this listener thereby preventing a memory leak.
-   */
-  unsubscribe: () => any,
-}
-
-export type UpdatableObject<S, F extends FindOrFilter, Q extends QueryStatus> = {
-  replace: (replacement: S) => void;
-  patch: (patch: Partial<S>) => void;
-}
-  & { [K in keyof S]: S[K] extends Array<any> ? UpdatableArray<S[K], 'isFilter', 'notQueried'> : S[K] extends object ? UpdatableObject<S[K], F, Q> : UpdatablePrimitive<S[K], F, Q> }
-  & Readable<S, F>;
-
-export type UpdatableArray<S extends Array<any>, F extends FindOrFilter, Q extends QueryStatus> = {
-  // replace: (replacement: F extends 'isFilter' ? S : S[0]) => void,
-}
-  & (Q extends 'queried' ? {
-    or: Comparators<S, S[0], F> & (S[0] extends object ? SearchableAny<S, S[0], F> : {}),
-    and: Comparators<S, S[0], F> & (S[0] extends object ? SearchableAny<S, S[0], F> : {}),
-    replace: (replacement: F extends 'isFilter' ? S : S[0]) => void,
-    remove: () => void,
-  } : {
-    find: Comparators<S, S[0], 'isFind'> & (S[0] extends object ? SearchableAny<S, S[0], 'isFind'> : {}),
-    filter: Comparators<S, S[0], 'isFilter'> & (S[0] extends object ? SearchableAny<S, S[0], 'isFilter'> : {}),
-    removeAll: () => void,
-    replaceAll: (newArray: S) => void,
-  })
-  & (S[0] extends Array<any> ? {} : S[0] extends object ? UpdatableObject<S[0], F, Q> : UpdatablePrimitive<S[0], F, Q>);
-
-export type UpdatablePrimitive<S, F extends FindOrFilter, Q extends QueryStatus> = (
-  Q extends 'notQueried' ? {
-    replaceAll: (replacement: S) => void;
-  } : {
-    replace: (replacement: S) => void;
-  }
-) & (S extends number ?
-  (Q extends 'notQueried' ? {
-    incrementAll: (by: number) => void;
-  } : {
-    increment: (by: number) => void;
-  }) : {
-  }
-) & Readable<S, F>;
-
-export interface Readable<S, F extends FindOrFilter> {
-  read: () => F extends 'isFilter' ? DeepReadonly<S[]> : DeepReadonly<S>;
-  onChange: (changeListener: (state: F extends 'isFilter' ? DeepReadonly<S[]> : DeepReadonly<S>) => any) => Unsubscribable;
-}
-
-export type UpdatableAny<T, F extends FindOrFilter, Q extends QueryStatus>
-  = T extends Array<any>
-  ? UpdatableArray<T, F, Q>
-  : T extends object
-  ? UpdatableObject<T, F, Q>
-  : UpdatablePrimitive<T, F, Q>;
-
-export type Comparators<T, S, F extends FindOrFilter> = {
-  eq: (value: S) => UpdatableAny<T, F, 'queried'>,
-  ne: (value: S) => UpdatableAny<T, F, 'queried'>,
-  in: (array: S[]) => UpdatableAny<T, F, 'queried'>,
-  ni: (array: S[]) => UpdatableAny<T, F, 'queried'>,
-} & (S extends string ? {
-  match: (matches: RegExp) => UpdatableAny<T, F, 'queried'>,
-  gt: (greaterThan: S) => UpdatableAny<T, F, 'queried'>,
-  gte: (greaterThanOrEqualTo: S) => UpdatableAny<T, F, 'queried'>,
-  lt: (lessThan: S) => UpdatableAny<T, F, 'queried'>,
-  lte: (lessThanOrEqualTo: S) => UpdatableAny<T, F, 'queried'>,
-} : S extends number ? {
-  gt: (greaterThan: S) => UpdatableAny<T, F, 'queried'>,
-  gte: (greaterThanOrEqualTo: S) => UpdatableAny<T, F, 'queried'>,
-  lt: (lessThan: S) => UpdatableAny<T, F, 'queried'>,
-  lte: (lessThanOrEqualTo: S) => UpdatableAny<T, F, 'queried'>,
-} : {});
-
-export type SearchableAny<T, S, F extends FindOrFilter> = { [K in keyof S]: (S[K] extends object ? (SearchableAny<T, S[K], F> & Comparators<T, S[K], F>) : Comparators<T, S[K], F>) };
 
 export const createApplicationStore = <S>(
   initialState: S, options: { name: string } = { name: document.title }
@@ -113,13 +12,6 @@ export const createApplicationStore = <S>(
 export const libState = {
   appStates: {} as { [name: string]: any },
   logLevel: 'none' as ('debug' | 'none'),
-}
-
-export interface StateAction {
-  type: (state: any) => 'property' | 'search' | 'comparator' | 'action';
-  name: string;
-  arg: any;
-  actionType: string | null;
 }
 
 export const readSelector = (storeName: string) => {
@@ -155,35 +47,6 @@ export const readSelector = (storeName: string) => {
     });
   };
   return initialize({}, true, []);
-}
-
-export const compare = (arg0: any, arg1: any, comparator: string) => {
-  if (comparator === 'eq') {
-    return arg0 === arg1
-  } else if (comparator === 'in') {
-    return arg1.includes(arg0);
-  }
-}
-
-export const readState = <S>(state: S, stateActions: StateAction[]): any => {
-  const action = stateActions.shift();
-  if (!action) { return; /* Logically impossible */ }
-  if (stateActions.length > 0) {
-    if (['find', 'filter'].includes(action.name)) {
-      const queryPath = stateActions.shift();
-      if (!queryPath) { return; /* Note: POSSIBLE! */ }
-      const argAction = stateActions.shift();
-      if (!argAction) { return; /* Logically impossible */ }
-      // return (oldObj as any as any[]).map((e, i) => e[queryPath.name] === argAction.arg
-      //   ? { ...(oldObj as any)[i], ...copyState((oldObj as any)[i] || {}, (newObj as any)[i] || {}, stateActions) }
-      //   : e);
-      return readState(((state || []) as any[])[action.name as any]((e: any) => compare(e[queryPath.name], argAction.arg, argAction.name)), stateActions.slice());
-    } else {
-      return readState((state as any)[action.name], stateActions.slice());
-    }
-  } else {
-    return state;
-  }
 }
 
 export const writeState = (oldObj: any, newObj: any, stateActions: StateAction[]): any => {
@@ -260,5 +123,31 @@ export const writeState = (oldObj: any, newObj: any, stateActions: StateAction[]
   }
 }
 
+export const readState = <S>(state: S, stateActions: StateAction[]): any => {
+  const action = stateActions.shift();
+  if (!action) { return; /* Logically impossible */ }
+  if (stateActions.length > 0) {
+    if (['find', 'filter'].includes(action.name)) {
+      const queryPath = stateActions.shift();
+      if (!queryPath) { return; /* Note: POSSIBLE! */ }
+      const argAction = stateActions.shift();
+      if (!argAction) { return; /* Logically impossible */ }
+      // return (oldObj as any as any[]).map((e, i) => e[queryPath.name] === argAction.arg
+      //   ? { ...(oldObj as any)[i], ...copyState((oldObj as any)[i] || {}, (newObj as any)[i] || {}, stateActions) }
+      //   : e);
+      return readState(((state || []) as any[])[action.name as any]((e: any) => compare(e[queryPath.name], argAction.arg, argAction.name)), stateActions.slice());
+    } else {
+      return readState((state as any)[action.name], stateActions.slice());
+    }
+  } else {
+    return state;
+  }
+}
 
-
+export const compare = (arg0: any, arg1: any, comparator: string) => {
+  if (comparator === 'eq') {
+    return arg0 === arg1
+  } else if (comparator === 'in') {
+    return arg1.includes(arg0);
+  }
+}
---
+++ src/types.ts
@@ -0,0 +1,107 @@
+export type FindOrFilter = 'isFind' | 'isFilter';
+export type QueryStatus = 'notQueried' | 'queried';
+
+/**
+ * An array which cannot be mutated
+ */
+export interface DeepReadonlyArray<T> extends ReadonlyArray<DeepReadonly<T>> {
+}
+
+/**
+ * An object which cannot be mutated
+ */
+export type DeepReadonlyObject<T> = {
+  readonly [P in keyof T]: DeepReadonly<T[P]>;
+};
+
+/**
+ * An object or an array which cannot be mutated
+ */
+export type DeepReadonly<T> =
+  T extends (infer R)[] ? DeepReadonlyArray<R> :
+  T extends object ? DeepReadonlyObject<T> :
+  T;
+
+/**
+ * An object which can be unsubscribed from
+ */
+export interface Unsubscribable {
+  /**
+   * Unsubscribes from this listener thereby preventing a memory leak.
+   */
+  unsubscribe: () => any,
+}
+
+export type UpdatableObject<S, F extends FindOrFilter, Q extends QueryStatus> = {
+  replace: (replacement: S) => void;
+  patch: (patch: Partial<S>) => void;
+}
+  & { [K in keyof S]: S[K] extends Array<any> ? UpdatableArray<S[K], 'isFilter', 'notQueried'> : S[K] extends object ? UpdatableObject<S[K], F, Q> : UpdatablePrimitive<S[K], F, Q> }
+  & Readable<S, F>;
+
+export type UpdatableArray<S extends Array<any>, F extends FindOrFilter, Q extends QueryStatus> = (Q extends 'queried' ? {
+    or: Comparators<S, S[0], F> & (S[0] extends object ? SearchableAny<S, S[0], F> : {}),
+    and: Comparators<S, S[0], F> & (S[0] extends object ? SearchableAny<S, S[0], F> : {}),
+    replace: (replacement: F extends 'isFilter' ? S : S[0]) => void,
+    remove: () => void,
+  } : {
+    find: Comparators<S, S[0], 'isFind'> & (S[0] extends object ? SearchableAny<S, S[0], 'isFind'> : {}),
+    filter: Comparators<S, S[0], 'isFilter'> & (S[0] extends object ? SearchableAny<S, S[0], 'isFilter'> : {}),
+    removeAll: () => void,
+    replaceAll: (newArray: S) => void,
+  })
+  & (S[0] extends Array<any> ? {} : S[0] extends object ? UpdatableObject<S[0], F, Q> : UpdatablePrimitive<S[0], F, Q>);
+
+export type UpdatablePrimitive<S, F extends FindOrFilter, Q extends QueryStatus> = (
+  Q extends 'notQueried' ? {
+    replaceAll: (replacement: S) => void;
+  } : {
+    replace: (replacement: S) => void;
+  }
+) & (S extends number ?
+  (Q extends 'notQueried' ? {
+    incrementAll: (by: number) => void;
+  } : {
+    increment: (by: number) => void;
+  }) : {
+  }
+) & Readable<S, F>;
+
+export interface Readable<S, F extends FindOrFilter> {
+  read: () => F extends 'isFilter' ? DeepReadonly<S[]> : DeepReadonly<S>;
+  onChange: (changeListener: (state: F extends 'isFilter' ? DeepReadonly<S[]> : DeepReadonly<S>) => any) => Unsubscribable;
+}
+
+export type UpdatableAny<T, F extends FindOrFilter, Q extends QueryStatus>
+  = T extends Array<any>
+  ? UpdatableArray<T, F, Q>
+  : T extends object
+  ? UpdatableObject<T, F, Q>
+  : UpdatablePrimitive<T, F, Q>;
+
+export type Comparators<T, S, F extends FindOrFilter> = {
+  eq: (value: S) => UpdatableAny<T, F, 'queried'>,
+  ne: (value: S) => UpdatableAny<T, F, 'queried'>,
+  in: (array: S[]) => UpdatableAny<T, F, 'queried'>,
+  ni: (array: S[]) => UpdatableAny<T, F, 'queried'>,
+} & (S extends string ? {
+  match: (matches: RegExp) => UpdatableAny<T, F, 'queried'>,
+  gt: (greaterThan: S) => UpdatableAny<T, F, 'queried'>,
+  gte: (greaterThanOrEqualTo: S) => UpdatableAny<T, F, 'queried'>,
+  lt: (lessThan: S) => UpdatableAny<T, F, 'queried'>,
+  lte: (lessThanOrEqualTo: S) => UpdatableAny<T, F, 'queried'>,
+} : S extends number ? {
+  gt: (greaterThan: S) => UpdatableAny<T, F, 'queried'>,
+  gte: (greaterThanOrEqualTo: S) => UpdatableAny<T, F, 'queried'>,
+  lt: (lessThan: S) => UpdatableAny<T, F, 'queried'>,
+  lte: (lessThanOrEqualTo: S) => UpdatableAny<T, F, 'queried'>,
+} : {});
+
+export type SearchableAny<T, S, F extends FindOrFilter> = { [K in keyof S]: (S[K] extends object ? (SearchableAny<T, S[K], F> & Comparators<T, S[K], F>) : Comparators<T, S[K], F>) };
+
+export interface StateAction {
+  type: (state: any) => 'property' | 'search' | 'comparator' | 'action';
+  name: string;
+  arg: any;
+  actionType: string | null;
+}
