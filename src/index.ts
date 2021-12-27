7e310d493fae17e4068d7353f9c678ca48a3f33b
stephen <stephensimonpaul@gmail.com>
2021-12-27 18:42:48 +0200

    ditto

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,2 @@
-export { createStore } from './core';
-export { augment } from './augment';
-export { derive } from './derive';
-export { transact } from './transact';
-export { nestStoreIfPossible } from './nest';
-export { trackWithReduxDevtools } from './redux-devtools';
-export { mergeStoreIfPossible } from './merge';
-export { getStoreByName, listenToDevtoolsDispatch } from './utility';
+
 export * from './type';
