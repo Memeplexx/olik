2808c39221400a052f4afd62a460dcb64af48cd5
stephen <stephensimonpaul@gmail.com>
2022-01-02 18:08:28 +0200

    Deploy website - based on af10cfe9bc2a1bb148631eaac970f869f12839cc

--- src/transact.ts
+++
@@ -1,9 +0,0 @@
-import { libState } from './constant';
-
-export const transact = (...operations: (() => void)[]) => {
-  if (!operations.length) { return; }
-  if (operations.length === 1) { return operations[0](); }
-  libState.isInsideTransaction = true;
-  operations.forEach(op => op());
-  libState.isInsideTransaction = false;
-}
