a82dd948fad79f964304178ea130b4b05c872002
Stephen Paul <stephensimonpaul@gmail.com>
2022-01-24 10:56:05 +0200

    Update docs for svelte

--- test/array-deep.spec.ts
+++ test/array-deep.spec.ts
@@ -12,60 +12,60 @@ describe('array-deep', () => {
 
   it('should select.arr.find.id.eq(2).patch({ val: 1 })', () => {
     const state = { arr: [{ id: 1, val: 0, obj: { num: 0 } }, { id: 2, val: 0, obj: { num: 0 } }], obj: { num: 0 } };
-    const select = createStore({ name, state });
+    const store = createStore({ name, state });
     const payload = { val: 1 };
-    select.arr
+    store.arr
       .find.id.eq(2)
       .patch(payload);
-    expect(select.state).toEqual({ ...state, arr: [state.arr[0], { ...state.arr[1], ...payload }] });
+    expect(store.state).toEqual({ ...state, arr: [state.arr[0], { ...state.arr[1], ...payload }] });
   })
 
   it('should select.arr.find.id.eq(2).replace({ id: 4, val: 2 })', () => {
     const state = { arr: [{ id: 1, val: 0 }, { id: 2, val: 0 }], obj: { num: 0 } };
-    const select = createStore({ name, state });
-    const stateBefore = select.state;
+    const store = createStore({ name, state });
+    const stateBefore = store.state;
     const payload = { id: 4, val: 2 };
-    select.arr
+    store.arr
       .find.id.eq(2)
       .replace(payload);
-    expect(currentAction(select)).toEqual({
+    expect(currentAction(store)).toEqual({
       type: 'arr.find.id.eq(2).replace()',
       payload,
     });
-    const stateAfter = select.state;
+    const stateAfter = store.state;
     expect(stateBefore).not.toEqual(stateAfter);
     expect(stateBefore.obj).toEqual(stateAfter.obj);
     expect(stateBefore.arr).not.toEqual(stateAfter.arr);
-    expect(select.arr.find.id.eq(1).state).toEqual(stateBefore.arr.find(e => e.id === 1));
-    expect(select.state).toEqual({ arr: [{ id: 1, val: 0 }, { id: 4, val: 2 }], obj: { num: 0 } });
+    expect(store.arr.find.id.eq(1).state).toEqual(stateBefore.arr.find(e => e.id === 1));
+    expect(store.state).toEqual({ arr: [{ id: 1, val: 0 }, { id: 4, val: 2 }], obj: { num: 0 } });
   })
 
   it('should select.arr.filter.id.in([1, 2]).patch({ val: 1 })', () => {
     const state = { arr: [{ id: 1, val: 0 }, { id: 2, val: 0 }, { id: 3, val: 0 }] };
-    const select = createStore({ name, state });
+    const store = createStore({ name, state });
     const payload = { val: 1 };
-    select.arr
+    store.arr
       .filter.id.in([1, 2])
       .patch(payload);
-    expect(currentAction(select)).toEqual({
+    expect(currentAction(store)).toEqual({
       type: 'arr.filter.id.in(1,2).patch()',
       payload,
     });
-    expect(select.state).toEqual({ arr: [{ id: 1, val: 1 }, { id: 2, val: 1 }, { id: 3, val: 0 }] });
+    expect(store.state).toEqual({ arr: [{ id: 1, val: 1 }, { id: 2, val: 1 }, { id: 3, val: 0 }] });
   })
 
   it('should find an element and replace one of its properties', () => {
     const state = { arr: [{ id: 1, val: 0, obj: { num: 0 } }, { id: 2, val: 0, obj: { num: 0 } }], obj: { num: 0 } };
-    const select = createStore({ name, state });
+    const store = createStore({ name, state });
     const payload = 1;
-    select.arr
+    store.arr
       .find.id.eq(2).val
       .replace(payload);
-    expect(currentAction(select)).toEqual({
+    expect(currentAction(store)).toEqual({
       type: 'arr.find.id.eq(2).val.replace()',
       payload,
     });
-    expect(select.state).toEqual({
+    expect(store.state).toEqual({
       ...state,
       arr: [
         state.arr[0],
@@ -79,17 +79,17 @@ describe('array-deep', () => {
 
   it('should find an element, find an element in the property array, and replace one if its properties', () => {
     const state = { arr: [{ id: 1, val: 0, arr: [{ id: 1, num: 1 }, { id: 2, num: 2 }] }, { id: 2, val: 0, arr: [{ id: 1, num: 1 }, { id: 2, num: 2 }] }], obj: { num: 0 } };
-    const select = createStore({ name, state });
+    const store = createStore({ name, state });
     const payload = 9;
-    select.arr
+    store.arr
       .find.id.eq(2)
       .arr.find.id.eq(1).num
       .replace(payload);
-    expect(currentAction(select)).toEqual({
+    expect(currentAction(store)).toEqual({
       type: 'arr.find.id.eq(2).arr.find.id.eq(1).num.replace()',
       payload,
     });
-    expect(select.state).toEqual({
+    expect(store.state).toEqual({
       ...state,
       arr: [
         state.arr[0], {
@@ -106,17 +106,17 @@ describe('array-deep', () => {
 
   it('should find an element, filter elements in the property array, and all of its properties', () => {
     const state = { arr: [{ id: 1, val: 0, arr: [{ id: 1, num: 1 }, { id: 2, num: 2 }] }, { id: 2, val: 0, arr: [{ id: 1, num: 1 }, { id: 2, num: 2 }] }], obj: { num: 0 } };
-    const select = createStore({ name, state });
+    const store = createStore({ name, state });
     const payload = 1;
-    select.arr
+    store.arr
       .find.id.eq(2)
       .arr.filter.id.in([1, 2]).num
       .increment(payload);
-    expect(currentAction(select)).toEqual({
+    expect(currentAction(store)).toEqual({
       type: 'arr.find.id.eq(2).arr.filter.id.in(1,2).num.increment()',
       payload,
     });
-    expect(select.state).toEqual({
+    expect(store.state).toEqual({
       ...state,
       arr: [
         state.arr[0], {
@@ -131,17 +131,17 @@ describe('array-deep', () => {
 
   it('should find an element, filter elements in the property array, and all of its properties', () => {
     const state = { arr: [{ id: 1, val: 0, arr: [{ id: 1, num: 1 }, { id: 2, num: 2 }] }, { id: 2, val: 0, arr: [{ id: 1, num: 1 }, { id: 2, num: 2 }] }], obj: { num: 0 } };
-    const select = createStore({ name, state });
+    const store = createStore({ name, state });
     const payload = 1;
-    select.arr
+    store.arr
       .find.id.eq(2)
       .arr.num
       .incrementAll(payload);
-    expect(currentAction(select)).toEqual({
+    expect(currentAction(store)).toEqual({
       type: 'arr.find.id.eq(2).arr.num.incrementAll()',
       payload,
     });
-    expect(select.state).toEqual({
+    expect(store.state).toEqual({
       ...state,
       arr: [
         state.arr[0], {
