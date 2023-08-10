a544e56c848677a4a81a365c52f474ce47867cf0
Stephen Paul <stephensimonpaul@gmail.com>
2023-08-10 17:42:48 +0200

    Tidy up

--- src/derive.ts
+++ src/derive.ts
@@ -3,26 +3,9 @@ import { Derivation, DerivationCalculationInputs, Readable, Unsubscribe } from '
 
 
 export function derive<X extends Readable<unknown>[]>(...args: X) {
-  // let previousParams = new Array<unknown>();
-  // let previousResult = null as unknown;
   let cacheKey: unknown[];
   return {
     $with: <R>(calculation: (...inputs: DerivationCalculationInputs<X>) => R) => {
-
-      // const getValue = () => {
-      //   const params = args.map(arg => arg.$state) as DerivationCalculationInputs<X>;
-      //   if (previousParams.length && params.every((v, i) => {
-      //     // Start with a simple equality check.
-      //     // Else, if an array has been filtered (creating a new array to be created each time) compare stringified versions of the state
-      //     return (v === previousParams[i]) || (Array.isArray(v) && JSON.stringify(v) === JSON.stringify(previousParams[i]));
-      //   })) {
-      //     return previousResult as R;
-      //   }
-      //   const result = calculation(...params);
-      //   previousParams = params;
-      //   previousResult = result;
-      //   return result;
-      // }
       const getValue = () => {
         const params = args.map(arg => arg.$state) as DerivationCalculationInputs<X>;
         const kvp = [...libState.derivations.entries()].find(([previousParams]) => {
@@ -41,21 +24,14 @@ export function derive<X extends Readable<unknown>[]>(...args: X) {
         cacheKey = params;
         return result;
       }
-
       const changeListeners = new Set<(value: R) => unknown>();
       const result = (new class {
         get $state() { return getValue(); }
-
-        // $invalidate = () => previousParams.length = 0;
         $invalidate = () => libState.derivations.delete(cacheKey);
-
         $onChange = (listener: (value: R) => unknown) => {
           changeListeners.add(listener);
           const unsubscribes: Unsubscribe[] = args
-
-            // .map(ops => ops.$onChange(() => listener(getValue())));
             .map(ops => ops.$onChange(() => listener(getValue())));
-
           return {
             unsubscribe: () => {
               unsubscribes.forEach(u => u.unsubscribe());
