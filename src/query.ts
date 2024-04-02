3b74f45e8f70a394c58a50d3e445bd590b322349
Memeplexx <stephensimonpaul@gmail.com>
2024-04-02 22:19:08 +0200

    Refactor constructQuery function to improve readability and maintainability

--- src/query.ts
+++ src/query.ts
@@ -8,11 +8,13 @@ import { getStateOrStoreState } from './utility';
 export const constructQuery = (
   { cursor, stateActions }: { stateActions: ReadonlyArray<StateAction>, cursor: { index: number } }
 ) => {
-  const concatenateQueries = (queries: QuerySpec[]): QuerySpec[] => {
+  const recurse = (queries: QuerySpec[]): QuerySpec[] => {
     const constructQuery = () => {
-      const subStateActions = stateActions.slice(cursor.index, cursor.index + stateActions.slice(cursor.index).findIndex(sa => is.anyComparatorProp(sa.name)));
+      const nextComparatorIndex = stateActions.slice(cursor.index).findIndex(sa => is.anyComparatorProp(sa.name));
+      const subStateActions = stateActions.slice(cursor.index, cursor.index + nextComparatorIndex);
       cursor.index += subStateActions.length;
-      const comparator = stateActions[cursor.index++];
+      const comparator = stateActions[cursor.index];
+      cursor.index++
       return (e: unknown) => {
         const subProperty = subStateActions.reduce((prev, curr) => is.record(prev) ? prev[curr.name] : undefined, e);
         const comparatorName = comparator.name as keyof typeof comparisons;
@@ -20,35 +22,36 @@ export const constructQuery = (
         return comparisons[comparatorName](subProperty, getStateOrStoreState(comparator.arg));
       }
     }
+    const constructConcat = () => {
+      const type = stateActions[cursor.index].name;
+      return (is.anyUpdateFunction(type) || is.anyReadFunction(type) || !is.libArg(type)) ? '$last' : type as '$and' | '$or';
+    }
     queries.push({
       query: constructQuery(),
-      concat: (() => {
-        const type = stateActions[cursor.index].name;
-        return (is.anyUpdateFunction(type) || is.anyReadFunction(type) || !is.libArg(type)) ? '$last' : type as '$and' | '$or';
-      })(),
+      concat: constructConcat(),
     });
     if (is.libArg(stateActions[cursor.index].name, '$and', '$or')) {
       cursor.index++;
-      return concatenateQueries(queries);
+      return recurse(queries);
     }
     return queries;
   }
-  const queries = concatenateQueries([]);
+  const queries = recurse([]);
   const ors = new Array<(arg: unknown) => boolean>();
   const ands = new Array<(arg: unknown) => boolean>();
-  for (let i = 0; i < queries.length; i++) {
-    const previousClauseWasAnAnd = queries[i - 1] && queries[i - 1].concat === '$and';
-    if (queries[i].concat === '$and' || previousClauseWasAnAnd) {
-      ands.push(queries[i].query);
+  queries.forEach((query, i) => {
+    const previousClauseWasAnAnd = queries[i - 1]?.concat === '$and';
+    if (query.concat === '$and' || previousClauseWasAnAnd) {
+      ands.push(query.query);
     }
-    if ((queries[i].concat === '$or' || queries[i].concat === '$last') && ands.length) {
-      const andsCopy = ands.slice();
+    if ((query.concat === '$or' || query.concat === '$last') && ands.length) {
+      const andsCopy = [...ands];
       ors.push(el => andsCopy.every(and => and(el)));
       ands.length = 0;
     }
-    if (!(queries[i].concat === '$and') && !previousClauseWasAnAnd) {
-      ors.push(queries[i].query);
+    if (!(query.concat === '$and') && !previousClauseWasAnAnd) {
+      ors.push(query.query);
     }
-  }
+  });
   return (e: unknown) => ors.some(fn => fn(e));
 }
