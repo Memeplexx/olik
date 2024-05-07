232eb867e07e64eb416f59c969481a14ef4a9389
Memeplexx <stephensimonpaul@gmail.com>
2024-05-07 19:54:16 +0200

    Refactor readState function for improved readability and maintainability

--- src/read.ts
+++ src/read.ts
@@ -8,23 +8,21 @@ export const readState = (
   state: unknown, stateActions: StateAction[], cursor?: Cursor
 ): unknown => {
   if (!cursor) cursor = { index: 0 };
-  const stateAction = stateActions[cursor.index];
-  const type = stateAction?.name;
-  const arg = stateAction?.arg;
-  if (isArray(state) && !libPropMap[type])
+  const { name, arg } = stateActions[cursor.index];
+  if (isArray(state) && !libPropMap[name])
     return state.map((_, i) => readState(state[i], stateActions, { ...cursor }));
   cursor.index++;
   if (cursor.index === stateActions.length)
     return state;
   if (typeof(state) === 'object' && state !== null && !isArray(state))
-    return readState((state as ValidJsonObject)[type], stateActions, cursor);
-  if (type === '$at')
+    return readState((state as ValidJsonObject)[name], stateActions, cursor);
+  if (name === '$at')
     return readState((state as ValidJsonObject)[arg as number], stateActions, cursor);
-  if (type === '$distinct')
+  if (name === '$distinct')
     return [...new Set(state as ValidJsonArray)];
   const query = constructQuery(stateActions, cursor);
-  if (type === '$find')
+  if (name === '$find')
     return readState((state as ValidJsonArray).find(query), stateActions, cursor);
-  if (type === '$filter')
+  if (name === '$filter')
     return readState((state as ValidJsonArray).filter(query), stateActions, cursor);
 }
